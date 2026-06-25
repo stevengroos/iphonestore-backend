5fc73e8496349e4a93d14f1fdefd64c90c5f4574
stevengroos <stevengroos2@gmail.com>
2026-06-25 00:16:33 -0300

    Fix: Configuración estricta de CORS para localhost

--- app/main.py
+++ app/main.py
@@ -14,7 +14,9 @@ app = FastAPI(title="Catálogo API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
